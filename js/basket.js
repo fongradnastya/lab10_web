8cbdb3ca283563dddd9431a002f1b55a8c9a71e0
Anastasia Fongrad <fongradnastya@gmail.com>
2023-03-17 23:22:53 +0700

    Added empty basket img

--- js/basket.js
+++ js/basket.js
@@ -14,6 +14,18 @@ deleteItems.forEach((item)=>{
 
 let commonPrice = 0;
 
+function checkIsEmpty(){
+    if(counters.length == 0){
+        console.log("Empty!");
+        const img = document.querySelector(".empty");
+        const commonPrice = document.querySelector(".common-price");
+        const message = document.querySelector(".warning");
+        img.style.display = "block";
+        commonPrice.style.display = "none";
+        message.style.display = "block";
+    }
+}
+
 function setPrice(){
     commonPrice = 0;
     Array.prototype.forEach.call(counters, function(item){
@@ -23,9 +35,11 @@ function setPrice(){
         const itemPriceTag = card.querySelector(".card-price");
         let itemPrice = Number(itemPriceTag.textContent.trim().slice(0, -3));
         commonPrice += quantity * itemPrice;
-        basketPrise.textContent = commonPrice + " р.";
-        headerPrice.textContent = commonPrice + " р.";
+        
     });
+    basketPrise.textContent = commonPrice + " р.";
+    headerPrice.textContent = commonPrice + " р.";
+    checkIsEmpty();
 }
 
 Array.prototype.forEach.call(counters, function(item){
