47c6560091bdb04159ee580685735575774f70e9
Anastasia Fongrad <fongradnastya@gmail.com>
2023-03-18 13:45:56 +0700

    Added new input styles

--- js/form.js
+++ js/form.js
@@ -7,11 +7,26 @@ console.log(checkBox.checked);
 
 checkBox.addEventListener("change", function(e){
     const addres = form.deliveryAdress;
-    console.log(addres);
     if(checkBox.checked){
         addres.style.display = "block";
     }
     else{
         addres.style.display = "none";
     }
+});
+
+const phone = form.tel;
+const email = form.email;
+
+function checkInput(){
+    const phoneValue = phone.value.trim();
+    const emailValue = email.value.trim();
+    if(emailValue === ''){
+        console.log("Mistake");
+    }
+}
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
 });
